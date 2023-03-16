fe236e5841da1c935818d8756659790af77fb108
Abdullahi Abdiaziz yusuf <abdullahikawte2019@gmail.com>
2023-03-16 17:22:18 +0200

    files uploaded

---
+++ app.js
@@ -0,0 +1,58 @@
+const sliderImage = document.querySelector('.image-slider');
+const leftArrow = document.querySelector('.arrow-left');
+const rightArrow = document.querySelector(".arrow-right");
+const heading = document.querySelector('.caption h1');
+const description = document.querySelector('.caption p');
+
+
+const images = [
+  'architect.jpg',
+  'chicago.jpg',
+  'london.jpg'
+]
+
+const headings = [
+  'Our Design',
+  'Chicago',
+  'London'
+]
+
+const descriptions = [
+  "Make your hause awesome",
+  "place of music and songs",
+  'The city that never sleeps',
+];
+
+let id = 0;
+
+window.addEventListener('DOMContentLoaded', () => {
+  slider(id);
+})
+
+function slider(id) {
+  sliderImage.style.backgroundImage = `url(images/${images[id]})`;
+  sliderImage.classList.add('image-fade');
+
+  setTimeout(() => {
+    sliderImage.classList.remove('image-fade');
+  }, 550)
+
+  heading.innerText = headings[id];
+  description.innerText = descriptions[id];
+}
+
+rightArrow.addEventListener('click', function() {
+  id++;
+  if(id > images.length - 1) {
+    id = 0;
+  }
+  slider(id);
+})
+
+leftArrow.addEventListener('click', () => {
+  id--;
+  if(id < 0) {
+    id = images.length - 1
+  }
+  slider(id)
+})
